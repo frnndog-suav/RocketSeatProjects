224b1fc356ed9de7406ba3c31bd300192158824c
Fernando Goia <f97.goya@gmail.com>
2023-11-13 20:56:19 -0300

    Implementando estrutura base do projeto

---
+++ desafio-projeto-2/src/routes/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { DefaultLayout } from "../layouts/DefaultLayout";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
