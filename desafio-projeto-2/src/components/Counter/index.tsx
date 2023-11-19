6cccd777155106da9b50dc4a9dd3e076277f883c
Fernando Goia <f97.goya@gmail.com>
2023-11-19 20:30:59 -0300

    Implementando lógica para aumentar e diminuir a quantidade de um item no componente 'Counter'

--- desafio-projeto-2/src/components/Counter/index.tsx
+++ desafio-projeto-2/src/components/Counter/index.tsx
@@ -1,14 +1,27 @@
 import { Minus, Plus } from "@phosphor-icons/react";
 import { CounterContainer } from "./styles";
+import { useState } from "react";
 
 export const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const disabledButton = count <= 0;
+
+  function handleDecreaseQuantity() {
+    setCount((currentCount) => currentCount - 1);
+  }
+
+  function handleIncreaseQuantity() {
+    setCount((currentCount) => currentCount + 1);
+  }
+
   return (
     <CounterContainer>
-      <button>
+      <button onClick={handleDecreaseQuantity} disabled={disabledButton}>
         <Minus size={20} />
       </button>
-      <input type="number" placeholder="00" defaultValue={1} readOnly />
-      <button>
+      <input type="number" placeholder="00" value={count} readOnly min={0} />
+      <button onClick={handleIncreaseQuantity}>
         <Plus size={20} />
       </button>
     </CounterContainer>
