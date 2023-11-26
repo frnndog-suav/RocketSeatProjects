df4c576a15de1bce9746946da777d38d6652be15
Fernando Goia <f97.goya@gmail.com>
2023-11-25 22:11:25 -0300

    CFinalizando página de sucesso

--- desafio-projeto-2/src/pages/Success/index.tsx
+++ desafio-projeto-2/src/pages/Success/index.tsx
@@ -1,17 +1,61 @@
+import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
 import { useLocation } from "react-router-dom";
+import IllustrationImg from "../../assets/Illustration.svg";
 import { CheckoutFormData } from "../Checkout";
-import { SuccessContainer } from "./styles";
+import {
+  AddressInfoDisplayContainer,
+  ConfirmedDataContainer,
+  InfoDisplay,
+  MapPinIcon,
+  MoneyIcon,
+  SuccessContainer,
+  TimeIcon,
+} from "./styles";
 
 export function SuccessPage() {
   const { state } = useLocation();
   const { formData } = state;
   const data = formData as CheckoutFormData;
 
+  const addressFirstLine = `Entrega em ${data.address}, ${data.addressNumber}`;
+  const addressSecondLine = `${data.neighborhood} - ${data.city}, ${data.uf}`;
+
   return (
     <SuccessContainer>
-      <h1>Success Page</h1>
-      <p>teste</p>
-      <span>{data.address}</span>
+      <ConfirmedDataContainer>
+        <span>Uhu! Pedido confirmado</span>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+        <InfoDisplay>
+          <AddressInfoDisplayContainer>
+            <MapPinIcon>
+              <MapPin size={16} weight="fill" />
+            </MapPinIcon>
+            <div>
+              <span>{addressFirstLine}</span>
+              <p>{addressSecondLine}</p>
+            </div>
+          </AddressInfoDisplayContainer>
+          <AddressInfoDisplayContainer>
+            <TimeIcon>
+              <Timer size={16} weight="fill" />
+            </TimeIcon>
+            <div>
+              <span>Previsão de entrega</span>
+              <p>20 min - 30 min </p>
+            </div>
+          </AddressInfoDisplayContainer>
+          <AddressInfoDisplayContainer>
+            <MoneyIcon>
+              <CurrencyDollar size={16} />
+            </MoneyIcon>
+            <div>
+              <span>Pagamento na entrega</span>
+              <p>Teste</p>
+            </div>
+          </AddressInfoDisplayContainer>
+        </InfoDisplay>
+      </ConfirmedDataContainer>
+      <img src={IllustrationImg} alt="Pessoa andando numa lambreta" />
     </SuccessContainer>
   );
 }
--- desafio-projeto-2/src/pages/Success/styles.ts
+++ desafio-projeto-2/src/pages/Success/styles.ts
@@ -2,5 +2,102 @@ import { styled } from "styled-components";
 
 export const SuccessContainer = styled.main`
   display: flex;
+  align-items: center;
   padding: 7rem 0px;
+  width: 100%;
+  justify-content: center;
+  gap: 7rem;
+  padding: 10rem;
+
+  & img {
+    width: 30.75rem;
+    height: 18rem;
+  }
+
+  & span {
+    color: ${(props) => props.theme["yellow-dark"]};
+    font-size: 2rem;
+    line-height: 130%;
+    font-weight: 600;
+    font-family: "Baloo 2", sans-serif;
+  }
+
+  & p {
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-size: 1.25rem;
+    line-height: 130%;
+  }
+`;
+
+export const ConfirmedDataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InfoDisplay = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 2.5rem;
+  margin-top: 2.5rem;
+  border: double 1px transparent;
+  border-top-right-radius: 36px;
+  border-top-left-radius: 6px;
+  border-bottom-left-radius: 36px;
+  border-bottom-right-radius: 6px;
+  background-image: linear-gradient(white, white),
+    radial-gradient(
+      circle at top left,
+      ${(props) => props.theme.yellow},
+      ${(props) => props.theme.purple}
+    );
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+`;
+
+export const AddressInfoDisplayContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  & span {
+    font-family: "Roboto";
+    color: ${(props) => props.theme["base-text"]};
+    font-size: 1rem;
+    font-weight: 500;
+  }
+
+  & p {
+    color: ${(props) => props.theme["base-text"]};
+    font-size: 1rem;
+    font-weight: 600;
+  }
+
+  & div {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+  }
+`;
+
+const IconBackground = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+  border-radius: 999px;
+`;
+
+export const MapPinIcon = styled(IconBackground)`
+  background-color: ${(props) => props.theme.purple};
+  color: white;
+`;
+
+export const TimeIcon = styled(IconBackground)`
+  background-color: ${(props) => props.theme.yellow};
+  color: white;
+`;
+
+export const MoneyIcon = styled(IconBackground)`
+  background-color: ${(props) => props.theme["yellow-dark"]};
+  color: white;
 `;
